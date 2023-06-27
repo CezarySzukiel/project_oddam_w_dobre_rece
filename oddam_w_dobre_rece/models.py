bbcbad016082e9b0b08a52e90f416135e8467245
Cezary <cezary.szukiel@gmail.com>
2023-06-27 19:18:55 +0200

    cleaning models

--- oddam_w_dobre_rece/models.py
+++ oddam_w_dobre_rece/models.py
@@ -25,19 +25,6 @@ class Institution(models.Model):
     def __str__(self):
         return self.name
 
-# Stwórz model dla daru - Donation posiadający następujące atrybuty:
-# - quantity (liczba worków)
-# - categories (relacja ManyToMany do modelu Category)
-# - institution (klucz obcy do modelu Institution)
-# - address (ulica plus numer domu)
-# - phone_number (numer telefonu)
-# - city
-# - zip_code
-# - pick_up_date
-# - pick_up_time
-# - pick_up_comment
-# - user (klucz obcy do tabeli user; domyślna tabela zakładana przez django; może być Nullem, domyślnie Null).
-
 
 class Donation(models.Model):
     quantity = models.IntegerField()
@@ -51,4 +38,3 @@ class Donation(models.Model):
     pick_up_time = models.TimeField()
     pick_up_comment = models.TextField()
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, default=None)
-
