cadb03c4fb19aac95e69dc8747c12e88428ea3cd
Cezary <cezary.szukiel@gmail.com>
2023-06-27 17:33:09 +0200

    models Category & Institution

--- oddam_w_dobre_rece/models.py
+++ oddam_w_dobre_rece/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Institution(models.Model):
+    TYPE_CHOICES = (
+        ('fundacja', 'fundacja'),
+        ('organizacja pozarządowa', 'organizacja pozarządowa'),
+        ('zbiórka lokalna', 'zbiórka lokalna')
+    )
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.CharField(max_length=64, choices=TYPE_CHOICES, default='fundacja')
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
