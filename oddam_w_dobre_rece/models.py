f35988cd00f18cf603d1806c373653a23d58cd41
Cezary <cezary.szukiel@gmail.com>
2023-06-27 19:29:18 +0200

    some amendments

--- oddam_w_dobre_rece/models.py
+++ oddam_w_dobre_rece/models.py
@@ -27,13 +27,13 @@ class Institution(models.Model):
 
 
 class Donation(models.Model):
-    quantity = models.IntegerField()
+    quantity = models.PositiveSmallIntegerField()
     categories = models.ManyToManyField(Category)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     address = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=16)
     city = models.CharField(max_length=128)
-    zip_code = models.CharField(max_length=16)
+    zip_code = models.CharField(max_length=6)
     pick_up_date = models.DateField()
     pick_up_time = models.TimeField()
     pick_up_comment = models.TextField()
