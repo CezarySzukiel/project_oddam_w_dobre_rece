8295b44eafd08c30497c24f849a7e17b1cf8a174
Cezary <cezary.szukiel@gmail.com>
2023-07-13 10:42:10 +0200

    example settings

--- project_oddam/settings.py
+++ project_oddam/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@example.com'
+EMAIL_HOST_PASSWORD = 'your_email_password'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
